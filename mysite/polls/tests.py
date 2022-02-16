bee3d80f5188d37be1428652fabac6f0b923106b
zengxiaoliang <zengxiaoliang@holomatic.com>
2022-02-16 23:36:02 +0800

    Add: 1. Add testcases for IndexView and DetailView based on Client from TestCase lib
    Why: 1. Add testcases for IndexView and DetailView based on Client from
    TestCase lib
    
    How: 1. Add testcases for IndexView and DetailView based on Client from
    TestCase lib

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -4,7 +4,12 @@ from django.test import TestCase
 import datetime
 
 from django.utils import timezone
+from django.urls import reverse
 from .models import Question
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days = days)
+    return Question.objects.create(question_text = question_text, pub_date = time)
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
@@ -23,3 +28,45 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours = 23, minutes = 59, seconds = 59)
         recent_question = Question(pub_date = time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        question = create_question(question_text = 'Past question', days = -30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+    def test_future_question(self):
+        question = create_question(question_text = 'Future question', days = 30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_two_past_question(self):
+        question1 = create_question(question_text = 'Past question 1', days = -10)
+        question2 = create_question(question_text = 'Past question 2', days = -20)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question1, question2])
+
+    def test_past_future_question(self):
+        question1 = create_question(question_text = 'Past question', days = -10)
+        create_question(question_text = 'Future question', days = 10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question1])
+
+class QuestionDetailViewTests(TestCase):
+    def test_past_question(self):
+        question = create_question(question_text = 'Past question', days = -5)
+        url = reverse('polls:detail', args = (question.id, ))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_future_question(self):
+        question = create_question(question_text = 'Future question', days = 10)
+        url = reverse('polls:detail', args = (question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
